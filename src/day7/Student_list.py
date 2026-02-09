9b15243f13e4127c1c9de27ddca1dafb33ea6ab1
DILAN-654 <150329795+DILAN-654@users.noreply.github.com>
2026-02-09 12:38:31 +0530

    Delete src/day7 directory
    deleted  files

--- src/day7/Student_list.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("data/students.csv", "r") as file:
-    reader = csv.DictReader(file)
-    
-    print("Students who Passed:")
-    for row in reader:
-        if row["Status"] == "Pass":
-            print(row["Name"])
--- src/day7/journal_script.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name: ")
-daily_goal = input("Enter your daily goal: ")
-
-with open("journal.txt", "a") as file:
-    file.write(f"Name: {name}, Goal: {daily_goal}\n")
-
-print("Entry saved to journal.txt!")
